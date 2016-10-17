4af523fcfdf5b9d6c9b95dbca65471fa381aa24c
kriss0r <kriss@ceusmedia.de>
2016-10-17 14:40:41 +0200

    Update server demo script.

--- demo/Server/index.php
+++ demo/Server/index.php
@@ -1,5 +1,5 @@
 <?php
-(@include '../vendor/autoload.php') or die('Please use composer to install required packages.' . PHP_EOL);
+(@include '../../vendor/autoload.php') or die('Please use composer to install required packages.' . PHP_EOL);
 
 use \CeusMedia\REST\Server as Server;
 
