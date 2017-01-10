6f92a43e21d0cdc7ecf6e0491b06fcd0ec70d913
kriss0r <kriss@ceusmedia.de>
2017-01-10 14:39:20 +0100

    Improve support for HTTP Basic Authentication.

--- src/Client.php
+++ src/Client.php
@@ -210,6 +210,6 @@ class Client{
 		$encoded	= base64_encode( $username . ':' . $password );
 		$this->requestHeaders[]	= 'Authentication: Basic ' . $encoded;
 		curl_setopt( $this->handler, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
-		curl_setopt( $this->handler, CURLOPT_USERPWD, $this->username . ':' . $this->password );
+		curl_setopt( $this->handler, CURLOPT_USERPWD, $username . ':' . $password );
 	}
 }
