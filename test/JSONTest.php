67bd26f76af5ed476c5d94fbf3dfd1e90b9d5d4f
kriss0r <kriss@ceusmedia.de>
2023-11-13 03:06:50 +0100

    Add first unit test.

---
+++ test/JSONTest.php
@@ -0,0 +1,38 @@
+<?php
+use CeusMedia\Common\Net\HTTP\Response as HttpResponse;
+use CeusMedia\REST\Server\Format\FormatInterface;
+use CeusMedia\REST\Server\Format\JSON as JsonFormat;
+
+class JSONTest extends \PHPUnit\Framework\TestCase
+{
+	protected FormatInterface $format;
+	protected HttpResponse $response;
+
+	public function setUp(): void
+	{
+		$this->format	= new JsonFormat();
+		$this->response	= new HttpResponse();
+	}
+
+	public function testGetExtension(): void
+	{
+		self::assertEquals( '.json', $this->format->getExtension() );
+	}
+
+	public function testGetContentType(): void
+	{
+		self::assertEquals( 'application/json', $this->format->getContentType() );
+	}
+
+	public function testGetMimeTypes(): void
+	{
+		self::assertEquals( ['application/json'], $this->format->getMimeTypes() );
+	}
+
+	public function testTransform(): void
+	{
+		self::assertEquals( 1, $this->format->transform( $this->response, 1 ) );
+		self::assertEquals( '"1"', $this->format->transform( $this->response, '1' ) );
+		self::assertEquals( "[\n    1,\n    2,\n    3\n]", $this->format->transform( $this->response, [1, 2, 3] ) );
+	}
+}
